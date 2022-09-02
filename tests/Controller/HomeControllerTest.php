37c4ffb92716f15474bd8047467712613fff27e5
Idan-dev <cartaidan@gmail.com>
2022-09-02 16:19:43 +0200

    Start implementing functional tests for standard menu
    Start implementing tests for homepage;

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -3,7 +3,10 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HTTPFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
 
 class HomeControllerTest extends WebTestCase
 {
@@ -12,11 +15,32 @@ class HomeControllerTest extends WebTestCase
 	public function setUp(): void
 	{
 		$this->client = static::createClient();
+		$this->urlGenerator = $this->client->getContainer()->get("router.default");
+	}
+
+	private function setUpUser(): void
+	{
+		$this->userRepository = $this->client->getContainer()->get("doctrine.orm.entity_manager")->getRepository(User::class);
+		$this->user = $this->userRepository->findOneByEmail("test.user@test.com");
+		$this->client->loginUser($this->user);
 	}
 
 	public function testHomepageIsUp()
 	{
-		$urlGenerator = $this->client->getContainer()->get("router.default");
-		$this->client->request(Request::METHOD_GET, $urlGenerator->generate("home"));
+		$this->client->request(Request::METHOD_GET, $this->urlGenerator->generate("home"));
+		$this->assertResponseStatusCodeSame(Response::HTTP_OK);
+	}
+
+	public function testMenuShowsLoginPageWhenNotLoggedIn()
+	{
+		$crawler = $this->client->request(Request::METHOD_GET, $this->urlGenerator->generate("home"));
+		$this->assertSame(1, $crawler->filterXPath('//a[@class="main-navigation-link" and @href="/login"]')->count());
+	}
+
+	public function testMenuDoesntShowLoginPageWhenLoggedIn()
+	{
+		$this->setUpUser();
+		$crawler = $this->client->request(Request::METHOD_GET, $this->urlGenerator->generate("home"));
+		$this->assertSame(0, $crawler->filterXPath('//a[@class="main-navigation-link" and @href="/login"]')->count());
 	}
 }
